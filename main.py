2159b054c7a4cc2685d03aebc80e01e0add0239e
Denis <kopyloveden@gmail.com>
2024-06-06 18:52:54 +0500

    fix: add some spaces

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 import uvicorn
-
+    
 from . import crud, models, schemas 
 from .database import SessionLocal, engine 
 
@@ -25,10 +25,12 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail='Email already registered')
     return crud.create_user(db=db, user=user)
 
+
 @app.get('/users/', response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip, limit)
     return users
+
 
 @app.get('/users/{user_id}', response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
@@ -37,9 +39,11 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
     return db_user
 
+
 @app.post('/users/{user_id}/items/', response_model=schemas.Item)
 def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
     return crud.create_user_item(db, item, user_id)
+
 
 @app.get('/items/', response_model=list[schemas.Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
